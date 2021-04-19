ccd5cfdb51bfd6e72c634213bf1b8820fc8be320
Jackson Rosser <jacksonrosser@Jacksons-MacBook-Pro.local>
2021-04-19 15:57:47 -0600

    Finalizing

--- unit04/javascript/main.js
+++ unit04/javascript/main.js
@@ -1,84 +1,191 @@
-document.querySelector("#title").innerText = "Guessing Game"
-document.querySelector("#slogan").innerText = "A Fun Game for Everyone!"
 
 
-//pick a random number between 0 and 16
-let correctNumber = Math.floor(Math.random() * 15)
-correctNumber++
+function getRandomNumberInclusive(min, max) {
+    min = Math.ceil(min)
+    max = Math.floor(max)
+    return Math.floor(Math.random() * (max - min)) + min
+}
 
-//initialize variables before any guesses are made
-let userNum = false
-let totalGuesses = 0
-let gamerGuess = 0
+//build a switch that calls a month from 1-12
 
-
-//After ever guess, the number of guesses must increment and the guess iteself must be evaluated; 
-//if low, say so
-//if high, say so
-//if right on, say so, give reward, game over message
-
-function evalGuess() {
-    totalGuesses += 1
-    gamerGuess = document.querySelector('#guess').value
-
-    const feedback = document.querySelector('#feedback')
-    console.log(`${gamerGuess}, The correct number is ${correctNumber}, ${totalGuesses}`)
-
-    const attempts = document.querySelector("#attempts")
-
-
-    if (gamerGuess == correctNumber) {
-        feedback.innerText = `Right On! You win!` +`\n`+  `The corect number was ${correctNumber}`
-        //After winning, award a ribbon
-        giveAward()
-    } else if (gamerGuess > correctNumber && gamerGuess < 16) {
-        feedback.innerText = "Too high, try again"
-    } else if (gamerGuess < correctNumber && gamerGuess > 0) {
-        feedback.innerText = "Too low, try again"
-    } else {
-        feedback.innerText = "Please enter a " + '\n' + "NUMBER between" + '\n' + "1 and 15 and try again"
-        totalGuesses -= 1
+//Pick random number from 1-12
+let month = getRandomNumberInclusive(1, 12)
+//use that number to call a certain switch; 1=jan, 2=feb, etc.
+function getRandomMonth(month) {
+    let name
+    switch (month) {
+        case 1:
+            name = "January"
+            break;
+        case 2:
+            name = "February"
+            break;
+        case 3:
+            name = "March"
+            break;
+        case 4:
+            name = "April"
+            break;
+        case 5:
+            name = "May"
+            break;
+        case 6:
+            name = "June"
+            break;
+        case 7:
+            name = "July"
+            break;
+        case 8:
+            name = "August"
+            break;
+        case 9:
+            name = "September"
+            break;
+        case 10:
+            name = "October"
+            break;
+        case 11:
+            name = "November"
+            break;
+        case 12:
+            name = "December"
+        default:
+            name = "Not a month"
+            break;
     }
-
-
-    attempts.innerText = totalGuesses
-}
-
-function giveAward() {
-    console.log("congraduritos")
-    let imagePath="#"
-switch(totalGuesses){
-    
-    case 1:
-    case 2:
-    case 3:
-        if (awardImage.hasAttribute('src'===true)){
-            return;
-        }else{
-        awardImage.setAttribute('src', 'images/blueRibbon.png')
-        console.log("blue ribbon be urz")}
-        break;
-    case 4:
-    case 5:
-    case 6:
-        if (awardImage.hasAttribute('src'===true)){
-            return;
-        }else{
-        awardImage.setAttribute('src', 'images/redRibbon.png')
-        console.log('red ribon foi ya')}
-        break;
-    default:
-        if (awardImage.hasAttribute('src'===true)){
-            return;
-        }else{
-        console.log('yella ribzy iz heya')
-        awardImage.setAttribute('src', 'images/yellowRibbon.png')}
-}
+    return name
 
 }
-const awardImage = document.createElement('img')//creates an image tag with no src
-//awardImage.setAttribute('src', 'images/blueRibbon.png')
-const ribbon = document.querySelector('#ribbon')//selects ribbon div
-ribbon.appendChild(awardImage)//adds img tag to div element, now we just need to change the image src depending on the case
+//build switch that generates a day
+let day = getRandomNumberInclusive(1, 30)
 
-//if (awardImage.hasAttribute('src'===true)){
+function getDay(day) {
+    let title
+    switch (day) {
+        case 1:
+            let title = "1st"
+            break;
+        case 2:
+            let title = "2nd"
+            break;
+        case 3:
+            let title = "3rd"
+            break;
+        case 4:
+            let title = "4th"
+            break;
+        case 5:
+            let title = "5th"
+            break;
+        case 6:
+            let title = "6th"
+            break;
+        case 7:
+            let title = "7th"
+            break;
+        case 8:
+            let title = "8th"
+            break;
+        case 9:
+            let title = "9th"
+            break;
+        case 10:
+            let title = "10th"
+            break;
+        case 11:
+            let title = "11th"
+            break;
+        case 12:
+            let title = "12th"
+            break;
+        case 13:
+            let title = "13th"
+            break;
+        case 14:
+            let title = "14th"
+            break;
+        case 15:
+            let title = "15th"
+            break;
+        case 16:
+            let title = "16th"
+            break;
+        case 17:
+            let title = "17th"
+            break;
+        case 18:
+            let title = "18th"
+            break;
+        case 19:
+            let title = "19th"
+            break;
+        case 20:
+            let title = "20th"
+            break;
+        case 21:
+            let title = "21st"
+            break;
+        case 22:
+            let title = "22nd"
+            break;
+        case 23:
+            let title = "23rd"
+            break;
+        case 24:
+            let title = "24th"
+            break;
+        case 25:
+            let title = "25th"
+            break;
+        case 26:
+            let title = "26th"
+            break;
+        case 27:
+            let title = "27th"
+            break;
+        case 28:
+            let title = "28th"
+            break;
+        case 29:
+            let title = "29th"
+            break;
+        case 30:
+            let title = "30th"
+            break;
+        default:
+            let title = "Not a real day"
+            break;
+    }
+    return title
+}
+//function to determine one of five messages/fates
+let fate = getRandomNumberInclusive(1, 5)
+function getFortuneFate(fate) {
+    let message
+    switch (fate) {
+        case 1:
+            message = "gain treasure beyond measure!"
+            break;
+        case 2:
+            message = "become absolved from your guilt"
+            break;
+        case 3:
+            message = "find true love"
+            break;
+        case 4:
+            message = "secure millions of worthless doubloons in a dead currency"
+            break;
+        case 5:
+            message = "defeat your greatest foe"
+            break;
+        default:
+            break;
+    }
+}
+//call each function and parse the result into a message
+const monthName = getRandomMonth(month)
+const dayName = getDay(day)
+const fateMessage = getFortuneFate(fate)
+const fortuneRevealed = `On the ${dayName} of ${monthName}, you will ${fateMessage}`
+
+document.querySelector('#fortune').innerText = fortuneRevealed
