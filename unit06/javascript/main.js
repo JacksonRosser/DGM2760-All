555f25dfbf38095a7beb3b296f83ace347a408f5
Jackson Rosser <jacksonrosser@Jacksons-MacBook-Pro.local>
2021-04-22 16:38:17 -0600

    Finalized Everything

--- unit06/javascript/main.js
+++ unit06/javascript/main.js
@@ -28,7 +28,10 @@ console.log(verbList)
 
 
 const myStory = `Once upon a time there was an army of <span> ${adjectiveList[0]}</span><span> ${nounList[0]}</span> that would 
-wage a war within their <span> ${nounList[1]}</span>`
+wage a war within their <span> ${nounList[1]}</span>. That is, until the <span> ${nounList[4]}</span> king decided to <span> ${verbList[0]}</span>
+a <span> ${nounList[3]}</span> over all the <span> ${nounList[0]}</span>. Finally the <span> ${adjectiveList[1]}</span> army of <span> ${adjectiveList[2]}</span> <span> ${nounList[0]} </span>
+united against the <span> ${nounList[4]}</span> and demand that he <span> ${verbList[2]}</span> his issues. Finally, the <span> ${nounList[2]}</span> lord
+declared a treaty and now they all <span> ${verbList[1]}</span> in peace.`
 
 storyPlace = document.querySelector("#story").innerHTML = myStory
 }
