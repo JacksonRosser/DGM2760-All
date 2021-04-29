618d37473d12c37aa082220fb5190f8487f5c342
Jackson Rosser <jacksonrosser@Jacksons-MacBook-Pro.local>
2021-04-28 19:52:33 -0600

    Began Capstone, JSON and files are complete

--- Capstone/javascript/main.js
+++ Capstone/javascript/main.js
@@ -1,35 +1,36 @@
 // create an async function to deploy at an unconventional time
 
-async function getHotelData(){
+async function getPokeData(){
 
     try {
-        const response = await fetch('javascript/hotel.json')
+        const response = await fetch('javascript/pokemon.json')
         return await response.json() //return the JSON object 
     } catch (error) {
         console.error(error)}
 }
 
-let hotelData = {}
+let pokeData = {}
 
-getHotelData().then(data=> hotelData = data)
+getPokeData().then(data=> pokeData = data)
 
 
 var anchor = document.querySelectorAll('a')
-anchor.forEach(title => {title.addEventListener('click', hotelInfo)});
+anchor.forEach(title => {title.addEventListener('click', pokeInfo)});
 
 //make click handlers
-function hotelInfo(event){
+function pokeInfo(event){
     //find out what was clicked, match the name with JSON
     
-    let hotelChoice= hotelData.hotels.find(hotel=>{
-        return event.target.id === hotel.name.toLowerCase()
+    let pokeChoice= pokeData.starters.find(starters=>{
+        return event.target.id === starters.name.toLowerCase()
     })
-    console.log(hotelChoice)
-//I'm thinking something like  onclick of marriot button, document.querySelector(#rooms).textContent(hotelData[0].rooms), etc.
-    document.querySelector("#hotelName").textContent = `${hotelChoice.name}`
-    document.querySelector("#address").textContent= `  ${hotelChoice.address}`
-    document.querySelector("#rooms").textContent= `  ${hotelChoice.rooms}`
-    document.querySelector("#gym").textContent= `  ${hotelChoice.gym}`
-    document.querySelector("#type").textContent= `  ${hotelChoice.roomTypes}`
-    document.querySelector('#roomImage').setAttribute('src', `images/${hotelChoice.picture}`)
+    console.log(pokeChoice)
+    console.log(pokeChoice.name)
+    document.querySelector("#pokeName").textContent = `${pokeChoice.name}`
+    
+    document.querySelector("#type").textContent= `  ${pokeChoice.type}`
+    document.querySelector("#evolution").textContent= `  ${pokeChoice.evolution}`
+    document.querySelector("#pokedex").textContent= `  ${pokeChoice.pokedex}`
+    document.querySelector('#pokeImage').setAttribute('src', `images/${pokeChoice.name}.jpg`)
+    
 }
